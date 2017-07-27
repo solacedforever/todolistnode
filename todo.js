aa3052bc67526e2399d090c6ed4f0937e347ebd1
James Spargo <solacedforever@gmail.com>
2017-07-27 12:32:54 -0400

    changes

--- todo.js
+++ todo.js
@@ -10,16 +10,13 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
 app.use(expressValidator());
 
 app.get("/", function (req, res) {
   res.render('todo', { todos: listOfTodos});
 });
-
 app.post("/", function (req, res) {
   let id = parseInt(Math.random() * 1000);
   newTodoObject = { text: req.body.todo, id: id}
@@ -36,8 +33,6 @@ app.post('/mark-complete/:id', function(req, res) {
   res.redirect('/');
 
 });
-
-
 app.post('/', function(req, res) {
   var schema = {
     'name': {
@@ -53,12 +48,11 @@ app.post('/', function(req, res) {
   req.getValidationResult().then(function(result) {
     if (result.isEmpty()) {
       res.render('answers', { answers: req.body });
-    } else {
+      } else {
       res.render('todo', { errors: result.array() });
     }
   });
 });
-
 app.listen(3000, function() {
-  console.log('DAS A REAL NICE SERVER');
+  console.log('Server is up.');
 });

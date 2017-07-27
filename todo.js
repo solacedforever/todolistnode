5a8d4137512d6f18aaf07f78000c94aa2b54b8b9
James Spargo <solacedforever@gmail.com>
2017-07-27 10:40:53 -0400

    updates

--- todo.js
+++ todo.js
@@ -3,10 +3,10 @@ const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 var app = express();
 const expressValidator = require('express-validator');
-const todos = ["Wash the car"];
+let listOfTodos = [];
 
 app.engine('mustache',mustacheExpress());
-app.set('views','./views');
+app.set('views',__dirname + '/views');
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
@@ -17,29 +17,36 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(expressValidator());
 
 app.get("/", function (req, res) {
-  res.render('todo', { todos: todos });
+  res.render('todo', { todos: listOfTodos});
 });
 
 app.post("/", function (req, res) {
-  todos.push(req.body.todo);
+  let id = parseInt(Math.random() * 1000);
+  newTodoObject = { text: req.body.todo, id: id}
+  listOfTodos.push(newTodoObject);
   res.redirect('/');
-})
-
-
-
-app.get('/', function(req,res) {
-  res.render('todo');
 });
+
+app.post('/mark-complete/:id', function(req, res) {
+  let idOfTheTodoThatImMarkingComplete = parseInt(req.params.id);
+  let completeTodo = listOfTodos.find(function (todo) {
+    return todo.id === idOfTheTodoThatImMarkingComplete
+  });
+  completeTodo.complete = true
+  res.redirect('/');
+
+});
+
 
 app.post('/', function(req, res) {
   var schema = {
     'name': {
       notEmpty: true,
       isLength: {
-        options: [{ min: 100 }],
-        errorMessage: 'Name must not be longer than 100 characters'
+        options: [{ min: 2 }],
+        errorMessage: ' must be longer than 2 characters'
       },
-      errorMessage: 'Invalid Name'
+      errorMessage: 'Invalid Entry'
     }
   };
   req.assert(schema);
@@ -53,5 +60,5 @@ app.post('/', function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log('GOTCHU A SERVER UP');
+  console.log('DAS A REAL NICE SERVER');
 });
